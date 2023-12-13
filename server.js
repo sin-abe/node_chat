c267ac0830a4bc4121383ba89a7a81805794a041
220005 <220005ce@yse-c.net>
2023-12-13 10:23:00 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+// マルチバイト対応・/publicを公開
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+// サーバーの作成
+const { createServer, Server } = require('node:http');
+const server = createServer(app);
+
+// .envの読み込み
+const dotenv = require('dotenv');
+dotenv.config();
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+// サーバーを待機
+server.listen(port,host,() =>{
+    console.log(`listening on http://${host}:${port}`);
+})
+
+const io = new Server(server);
+io.on("connection",(socket) =>{
+    console.log("connected:" + socket.id);
+    socket.on("chat_message",(data) =>{
+        console.log(data);
+        io.emit("chat_message",data);
+    });
+});
